24c9db4c5c2ca26d518ccc76b9c265718061be34
fnzksxl <fnzksxl@gmail.com>
2023-11-20 16:54:00 +0900

    Papago 번역 기능 추가

--- app/AI/test.py
+++ app/AI/test.py
@@ -1,7 +1,11 @@
 from transformers import pipeline
+from dotenv import load_dotenv
 from deep_translator import GoogleTranslator
-
+import os
+import requests
 import app.AI.config as config
+
+load_dotenv()
 
 nlg_pipeline=pipeline('text2text-generation',model=config.model_path,tokenizer=config.model_name)
 
@@ -10,6 +14,28 @@ def generate_text(text, num_return_sequences=1, max_length=60):
   text = f"{target_style_name} 말투로 변환:{text}"
   out = nlg_pipeline(text, num_return_sequences=num_return_sequences, max_length=max_length)
   return [x['generated_text'] for x in out]
+
+def translate_with_papago(text, source_lang='ko', target_lang='en'):
+    url = "https://openapi.naver.com/v1/papago/n2mt"
+
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'X-Naver-Client-Id': os.environ.get("CLIENT_ID"),
+        'X-Naver-Client-Secret': os.environ.get("CLIENT_SECRET")
+    }
+
+    data = {
+        'source': source_lang,
+        'target': target_lang,
+        'text': text
+    }
+
+    response = requests.post(url, headers=headers, data=data)
+    translation_result = response.json()
+    translated_text = translation_result['message']['result']['translatedText']
+    
+    return translated_text
+
 
 def get_koen_text(ko_text):
   return GoogleTranslator(source='ko', target='en').translate(ko_text)

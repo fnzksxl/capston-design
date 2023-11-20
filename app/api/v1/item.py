5ca7a620db52d88dc465c5ba5921debaef02af02
fnzksxl <fnzksxl@gmail.com>
2023-11-20 16:55:12 +0900

    검색 기록 삭제 엔드포인트 작성

--- app/api/v1/item.py
+++ app/api/v1/item.py
@@ -19,10 +19,19 @@ async def get_item_list(db: Session = Depends(get_db)):
 
 @router.post("/add", response_model=schemas.ResourceId,status_code=status.HTTP_201_CREATED)
 async def add_item(data: schemas.TsItemAdd, cred: HTTPAuthorizationCredentials = Depends(security), db: Session = Depends(get_db)):
-  if utils.verify_user(cred):
+  if await utils.verify_user(cred):
     user_info = await utils.get_username(cred,db)
     row = models.TsItem(**data.dict(),owner_id=user_info.get('id'))
     db.add(row)
     db.commit()
     
     return row
+  
+@router.post("/delete",status_code=status.HTTP_202_ACCEPTED)
+async def delete_item(data: schemas.TsItemDelete, cred: HTTPAuthorizationCredentials = Depends(security), db: Session = Depends(get_db)):
+  if await utils.verify_user(cred):
+    item_info = db.query(models.TsItem).filter_by(id=data.item_id).first()
+    db.delete(item_info)
+    db.commit()
+
+    return {"success":True}

4673361ec98f80b493b3f72081968953c8448af2
fnzksxl <fnzksxl@gmail.com>
2023-11-21 15:15:36 +0900

    일본어, 중국어 번역 추가

--- app/api/v1/translate.py
+++ app/api/v1/translate.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm.session import Session
 from fastapi import Depends, APIRouter, HTTPException
 from fastapi.security import HTTPAuthorizationCredentials,HTTPBearer
 
-from app import models, schemas
+from app import schemas
 from app.database import get_db
 
 from app.AI import test
@@ -17,7 +17,8 @@ async def translate_item(data: schemas.ToTranslate,cred: HTTPAuthorizationCreden
   if await utils.verify_user(cred):
     translated_text = test.generate_text(data.dialect)[0] # Translation
     english_text = test.translate_with_papago(translated_text)
-
-    return {"dialect": data.dialect, "standard": translated_text, "english": english_text}
+    chinese_text = test.translate_with_papago(translated_text,target_lang='zh-CN')
+    japanese_text = test.translate_with_papago(translated_text,target_lang='ja')
+    return {"dialect": data.dialect, "standard": translated_text, "english": english_text, "chinese": chinese_text, "japanese": japanese_text}
   
   return HTTPException(501, "User not verified")

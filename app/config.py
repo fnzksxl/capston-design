b5d1deda92815af3c2d73062e650a72a093f11a3
fnzksxl <fnzksxl@gmail.com>
2023-12-16 21:48:00 +0900

    Test: Test를 위한 환경 설정

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings
+from typing import ClassVar
 from functools import lru_cache
 
 
@@ -18,14 +19,56 @@ class Settings(BaseSettings):
     CLIENT_ID: str
     CLIENT_SECRET: str
 
+    # Test Config
+    TESTING: ClassVar[bool] = False
+
     class Config:
+        extra = "ignore"
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+class testSettings(BaseSettings):
+    # DB
+    DB_TEST_USERNAME: str
+    DB_TEST_HOST: str
+    DB_TEST_PASSWORD: str
+    DB_TEST_NAME: str
+    DB_TEST_PORT: int
+
+    # CRED
+    SECRET_KEY: str
+    ALGORITHM: str
+
+    # PAPAGO
+    CLIENT_ID: str
+    CLIENT_SECRET: str
+
+    # Test Config
+    TESTING: ClassVar[bool] = True
+
+    class Config:
+        extra = "ignore"
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+class isMain(BaseSettings):
+    IS_MAIN: bool
+
+    class Config:
+        extra = "ignore"
         env_file = ".env"
         env_file_encoding = "utf-8"
 
 
 @lru_cache
 def get_settings():
-    return Settings()
+    if is_main.IS_MAIN:
+        return Settings()
+    else:
+        return testSettings()
 
 
+is_main = isMain()
 settings = get_settings()
--- app/database.py
+++ app/database.py
@@ -4,26 +4,43 @@ from sqlalchemy.orm import sessionmaker
 
 from app.config import settings
 
-engine = create_engine(
-  "mysql+pymysql://{username}:{password}@{host}:{port}/{name}".format(
-    username=settings.DB_USERNAME,
-    password=settings.DB_PASSWORD,
-    host=settings.DB_HOST,
-    port=settings.DB_PORT,
-    name=settings.DB_NAME,
-  )
-)
-SessionLocal = sessionmaker(
-    bind=engine,
-    autocommit=False,
-    autoflush=False,
-)
+if not settings.TESTING:
+    engine = create_engine(
+        "mysql+pymysql://{username}:{password}@{host}:{port}/{name}".format(
+            username=settings.DB_USERNAME,
+            password=settings.DB_PASSWORD,
+            host=settings.DB_HOST,
+            port=settings.DB_PORT,
+            name=settings.DB_NAME,
+        )
+    )
+    SessionLocal = sessionmaker(
+        bind=engine,
+        autocommit=False,
+        autoflush=False,
+    )
+else:
+    engine = create_engine(
+        "mysql+pymysql://{username}:{password}@{host}:{port}/{name}".format(
+            username=settings.DB_TEST_USERNAME,
+            password=settings.DB_TEST_PASSWORD,
+            host=settings.DB_TEST_HOST,
+            port=settings.DB_TEST_PORT,
+            name=settings.DB_TEST_NAME,
+        )
+    )
+    SessionLocal = sessionmaker(
+        bind=engine,
+        autocommit=False,
+        autoflush=False,
+    )
 
 Base = declarative_base()
 
+
 def get_db():
-  db = SessionLocal()
-  try:
-    yield db
-  finally:
-    db.close()
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
